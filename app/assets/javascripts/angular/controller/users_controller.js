fba86cb2d99f1d20d16b6fe7772a9fb59b738162
Pooja Saxena <saxenapooja03@gmail.com>
2017-05-26 14:49:43 +0530

    UI modified for controls

--- app/assets/javascripts/angular/controller/users_controller.js
+++ app/assets/javascripts/angular/controller/users_controller.js
@@ -32,7 +32,7 @@ myApp.controller("UserListCtr", ['$scope', '$http', '$resource', 'Users', 'User'
 }]);
 
 myApp.controller("UserUpdateCtr", ['$scope', '$resource', 'User', '$location', '$routeParams', function($scope, $resource, User, $location, $routeParams) {
-  $scope.user = User.get({id: $routeParams.id})
+  $scope.user = User.get({id: $routeParams.id});
   $scope.update = function(){
     if ($scope.userForm.$valid){
       User.update({id: $scope.user.id},{user: $scope.user},function(){
@@ -45,7 +45,7 @@ myApp.controller("UserUpdateCtr", ['$scope', '$resource', 'User', '$location', '
   
   $scope.addAddress = function(){
     $scope.user.addresses.push({street1: '', street2: '', city: '', state: '', country: '', zipcode: '' })
-  }
+  };
 
   $scope.removeAddress = function(index, user){
     var address = user.addresses[index];
@@ -59,7 +59,7 @@ myApp.controller("UserUpdateCtr", ['$scope', '$resource', 'User', '$location', '
 }]);
 
 myApp.controller("UserAddCtr", ['$scope', '$resource', 'Users', '$location', function($scope, $resource, Users, $location) {
-  $scope.user = {addresses: [{street1: '', street2: '', city: '', state: '', country: '', zipcode: '' }]}
+  $scope.user = {addresses: [{street1: '', street2: '', city: '', state: '', country: '', zipcode: '' }]};
   $scope.save = function () {
     if ($scope.userForm.$valid){
       Users.create({user: $scope.user}, function(){
@@ -68,11 +68,11 @@ myApp.controller("UserAddCtr", ['$scope', '$resource', 'Users', '$location', fun
         console.log(error)
       });
     }
-  }
+  };
 
   $scope.addAddress = function(){
-    $scope.user.addresses.push({street1: '', street2: '', city: '', state: '', country: '', zipcode: '' })
-  }
+    $scope.user.addresses.push({street1: '', street2: '', city: '', state: '', country: '', zipcode: '' });
+  };
 
   $scope.removeAddress = function(index, user){
     var address = user.addresses[index];
@@ -88,11 +88,18 @@ myApp.controller("UserAddCtr", ['$scope', '$resource', 'Users', '$location', fun
 
 myApp.controller("ContactController", ['$scope', function($scope) {
 //	$scope.contacts = ["hi@email.com", "hello@email.com"];
+	
 	$scope.contacts = [{"name":"Pooja", "email":"hi@email.com", "phone":"hello@email.com"}];
 	$scope.add = function() {
     	$scope.contacts.push($scope.newcontact);
     	$scope.newcontact = "";
-    }
+   };
+   
+   $scope.IsVisible = false;
+   
+   $scope.showControl = function(){
+   	$scope.IsVisible = $scope.IsVisible ? false : true;
+   };
 
 }]);
 
